10011b04252f6f1129cf4051f22ceb45935ec3f5
Witcher-MTM <phantomit228@gmail.com>
2023-02-26 12:30:35 +0200

    Calc fuel

--- fuel_app/App.js
+++ fuel_app/App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
 
 export default function App() {
+  const [fuel,setFuel] = useState(0);
+  const [km,setKm] = useState(0);
+  const [result,setResult] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titleText}>Fuel calc</Text>
+      <TextInput style={styles.text} onChangeText={setFuel} placeholder='Fuel'></TextInput>
+      <TextInput style={styles.text} onChangeText={setKm} placeholder='Kilometers'></TextInput>
+      <Button style={styles.btn} onPress={()=>setResult(parseInt((fuel/km)*100))} color='#3EBD30' title='Calc'></Button>
+      <Text style={styles.res}>It's your consumption: {result}</Text>
     </View>
   );
 }
@@ -16,5 +24,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    width:250,
+    borderStyle:'solid',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius:4, 
+    padding: 10,
+    backgroundColor:'#DADADA',
+  },
+  titleText:{
+    fontSize:18,
+    fontStyle:'italic',
+  },
+  res:{
+    marginTop:75,
+    fontSize:24,
+    color:'#3043BD',
   },
 });
